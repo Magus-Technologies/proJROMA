aae71f1924b5a1962cdaa4138ef52ac565c069d6
EmerRo <yarlequerodrigo9@gmail.com>
2026-07-30 08:41:43 -0500

    fix(api): corregir el token de APISPERU en la configuración

--- config/apisperu.php
+++ config/apisperu.php
@@ -2,5 +2,5 @@
 
 return [
     'url'   => env('APISPERU_URL', 'https://dniruc.apisperu.com/api/v1'),
-    'token' => env('APISPERU_TOKEN', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InN5c3RlbWNyYWZ0LnBlQGdtYWlsLmNvbSJ9.yuNS5hRaC0hCwymX_PjXRoSZJWLNNBeOdlLRSUGlHGA'),
+    'token' => env('APISPERU_TOKEN', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Ik1hbnVlbC5hZ3VhZG9AbWFndXN0ZWNobm9sb2dpZXMuY29tIn0.GQidyrlA0nN75zaMUstTR4a-XBUy4eH91VvX8YG2Wro'),
 ];
